d6902d19fdcf5ed8aa16b77ed32a7f6515b01866
l0o0 <linxzh1989@gmail.com>
2017-09-30 13:09:12 +0800

    Update config.

--- config.py
+++ config.py
@@ -15,39 +15,8 @@ class Config:
     PAGESIZE = 20
     FLASK_ADMIN = 'linxingzhong@1gene.com.cn'
     
-    
-    # jsondash config
-    JSONDASH_FILTERUSERS=False
-    JSONDASH_GLOBALDASH=True
-    JSONDASH_GLOBAL_USER='global'
+   
 
-    def _can_edit_global():
-        return True
-
-    def _can_delete():
-        return True
-
-    def _can_clone():
-        return True
-
-    def _get_username():
-        return 'anonymous'
-
-    JSONDASH = dict(
-            metadata=dict(
-                created_by=_get_username,
-                username=_get_username,
-                ),
-            static=dict(
-                js_path='js/vendor/',
-                css_path='css/vendor/',
-                ),
-            auth=dict(
-                edit_global=_can_edit_global,
-                clone=_can_clone,
-                delete=_can_delete,
-                )
-            )
 
 
     @staticmethod
